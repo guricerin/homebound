a1933b9aee0c62da1a42f2a6d30139d7f3b999a7
guricerin <chanbo1e9@gmail.com>
2025-04-09 11:00:56 +0900

    Revert "del fish"
    This reverts commit d7aec08925617ce7d525ed659c440eaf17133176.

---
+++ home/.config/fish/config.fish
@@ -0,0 +1,4 @@
+
+# starship: プロンプト改造
+## ↓は最終行に書くこと
+starship init fish | source
--- scripts/depends.sh
+++ scripts/depends.sh
@@ -9,5 +9,6 @@ brew install \
   starship \
   pre-commit \
   gitleaks \
+  fish \
   nushell
 $(brew --prefix)/opt/fzf/install
