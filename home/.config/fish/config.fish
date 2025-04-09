92641f1403b6def5ee62369a4cfd279ada527196
guricerin <chanbo1e9@gmail.com>
2025-04-09 17:01:24 +0900

    fish env var

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,3 +1,23 @@
+# PATH を通す際は fish_add_path を使用するのがよい
+## my bin
+fish_add_path $HOME/bin
+## Rust
+fish_add_path $HOME/.cargo/bin
+
+# PATH 以外の環境変数は set -gx (--global --export) を使用するのがよい
+## config
+set -gx XDG_CONFIG_HOME $HOME/.config
+
+## fzf
+set -gx FZF_DEFAULT_COMMAND "fd -H -E .git"
+## 検索結果はターミナルの下側に表示させる
+set -gx FZF_DEFAULT_OPTS "--reverse --height=90%"
+## CTRL + T でカレントディレクトリ以下のファイルをプレビュー表示しつつ曖昧検索
+set -gx FZF_CTRL_T_COMMAND "fd --type f -H -E .git"
+set -gx FZF_CTRL_T_OPTS '--preview "head -100 {}"'
+
+## Go
+set -gx GOPATH (go env GOPATH)
 
 # abbr: alias と違って、元のコマンドへと展開してくれる
 abbr --add l "ls -alF"
