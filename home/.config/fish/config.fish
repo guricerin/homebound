53570987a841c1896f9339f999b269b40ce83171
guricerin <chanbo1e9@gmail.com>
2025-04-09 15:38:44 +0900

    alias

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,4 +1,13 @@
 
+# abbr: alias と違って、元のコマンドへと展開してくれる
+abbr --add l "ls -alF"
+abbr --add g "git"
+abbr --add f "fzf"
+abbr --add k "kubectl"
+abbr --add tf "terraform"
+abbr --add d "docker"
+abbr --add dc "docker compose"
+
 # starship: プロンプト改造
 ## ↓は最終行に書くこと
 starship init fish | source
